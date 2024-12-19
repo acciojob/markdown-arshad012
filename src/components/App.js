2b0302220e5521db66437ae8f96dd1ff6fe3c35f
arshad012 <forarshad99@gmail.com>
2024-12-19 22:12:23 +0530

    Markdown complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,27 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React from "react";
+import '../styles/App.css';
+
+function App() {
+    const [loading, setLoading] = React.useState(true);
+    const [text, setText] = React.useState('');
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        },2000)
+    })
+
+    return loading ? <h1>Loading....</h1> :
+    <div className="app">
+        <div className="textarea-box">
+            <textarea className="textarea" onInput={(e) => setText(e.target.value)}></textarea>
+        </div>
+        <div className="preview">{text}</div>
+    </div>
+}
+
+
+export default App;
+
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
